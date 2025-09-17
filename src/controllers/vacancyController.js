22aac73280e9d51297c12fbd779928da0b14a826
arzummammadova <arzuam-af106@code.edu.az>
2025-09-17 14:03:34 +0400

    crud

--- src/controllers/vacancyController.js
+++ src/controllers/vacancyController.js
@@ -1,6 +1,4 @@
-
-
-import Vacancy from '../models/vacancyModel.js'
+import Vacancy from "../models/vacancyModel.js";
 export const postVacancy = async (req, res) => {
   try {
     const {
@@ -51,13 +49,61 @@ export const postVacancy = async (req, res) => {
   }
 };
 
-export const getVacancy=async(req,res)=>{
-    try {
-         const vacancies = await Vacancy.find().sort({ createdAt: -1 }); 
+export const getVacancy = async (req, res) => {
+  try {
+    const vacancies = await Vacancy.find().sort({ createdAt: -1 });
     return res.status(200).json(vacancies);
-        
-    } catch (error) {
-        return res.status(500).json({message:"internal server error",error:error.message})
-        
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "internal server error", error: error.message });
+  }
+};
+export const getVacancyById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const vacancy = await Vacancy.findById(id);
+
+    if (!vacancy) {
+      return res.status(404).json({ message: "Vacancy not found ❌" });
     }
+
+    return res.status(200).json(vacancy);
+  } catch (error) {
+    return res.status(500).json({
+      message: "Internal server error",
+      error: error.message,
+    });
+  }
+};
+
+export const deleteVacancyAll = async (req, res) => {
+  try {
+    const result = await Vacancy.deleteMany();
+    return res.status(200).json({
+      message: "deleted successfully",
+      deletedCount: result.deletedCount,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "internal server error",
+      error: error.message,
+    });
+  }
+};
+
+export const deleteVacancyById=async(req,res)=>{
+  try {
+    const {id}=req.params
+    const deletedVacancy=await Vacancy.findByIdAndDelete(id)
+    if(!deletedVacancy){
+     return res.status(404).json({message:"cannot find vacancy"})
+    }
+    return res.status(200).json({message:"deleted successfully"})
+    
+  } catch (error) {
+    return res.status(500).json({message:"internal serve error",error:error.message})
+    
+  }
+ 
 }
--- src/routers/vacancyRouter.js
+++ src/routers/vacancyRouter.js
@@ -1,9 +1,11 @@
-import { getVacancy, postVacancy } from "../controllers/vacancyController.js"
+import { deleteVacancyAll, deleteVacancyById, getVacancy, getVacancyById, postVacancy } from "../controllers/vacancyController.js"
 import express from "express"
 
 const vacancyrouter=express.Router()
 
 vacancyrouter.post('/',postVacancy)
 vacancyrouter.get('/',getVacancy)
-
+vacancyrouter.get('/:id',getVacancyById)
+vacancyrouter.delete('/',deleteVacancyAll)
+vacancyrouter.delete('/:id',deleteVacancyById)
 export  default vacancyrouter
