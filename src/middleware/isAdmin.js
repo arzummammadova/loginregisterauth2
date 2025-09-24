6a13f7f66747ed49dcaf17d739536988dd9d5407
arzummammadova <arzuam-af106@code.edu.az>
2025-09-24 17:26:01 +0400

    admin

---
+++ src/middleware/isAdmin.js
@@ -0,0 +1,9 @@
+export const isAdmin=(req,res,next)=>{
+    if(req.user.role=="admin"){
+        next()
+    }
+    else{
+        res.status(401).json({message:"You are not authorized to perform this action"})
+    }
+
+}
