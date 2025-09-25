ac41873748bb278460f9ff5950f4fc7607612fdf
arzummammadova <arzuam-af106@code.edu.az>
2025-09-26 00:04:47 +0400

    getusersbyid

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -35,3 +35,17 @@ export const deleteUser = async (req, res) => {
     }
   };
 
+export const getUserById = async (req, res) => {
+    try {
+        const {id}=req.params;
+        const user=await User.findById(id);
+        if(!user){
+            return res.status(404).json({message:"user not found"})
+        }
+        res.status(200).json(user)
+        
+    } catch (error) {
+        res.status(500).json({message:"internal server error"})
+        
+    }
+}
--- src/routers/adminRouter.js
+++ src/routers/adminRouter.js
@@ -1,13 +1,14 @@
 
 import express from 'express'
 import { isAdmin } from '../middleware/isAdmin.js'
-import { deleteUser, getAllUsers } from '../controllers/adminController.js'
+import { deleteUser, getAllUsers, getUserById } from '../controllers/adminController.js'
 import authMiddleware from '../middleware/authMiddleware.js'
 
 const adminRouter=express.Router()
 
 adminRouter.get('/users',authMiddleware,isAdmin,getAllUsers)
 adminRouter.delete('/users/:id',authMiddleware,isAdmin,deleteUser)
+adminRouter.get('/users/:id',authMiddleware,isAdmin,getUserById)
 
 
 
