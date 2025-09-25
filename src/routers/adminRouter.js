d369b95865fad6bddfe798593486bd188f24af66
arzummammadova <arzuam-af106@code.edu.az>
2025-09-25 23:52:39 +0400

    admin panel deelete

--- src/routers/adminRouter.js
+++ src/routers/adminRouter.js
@@ -1,12 +1,13 @@
 
 import express from 'express'
 import { isAdmin } from '../middleware/isAdmin.js'
-import { getAllUsers } from '../controllers/adminController.js'
+import { deleteUser, getAllUsers } from '../controllers/adminController.js'
 import authMiddleware from '../middleware/authMiddleware.js'
 
 const adminRouter=express.Router()
 
 adminRouter.get('/users',authMiddleware,isAdmin,getAllUsers)
+adminRouter.delete('/users/:id',authMiddleware,isAdmin,deleteUser)
 
 
 
