53204a78eaa43deaafa0a2a82fbf62ac9819142b
Arzu Mammadova <arzuam-af106@code.edu.az>
2026-01-15 16:33:47 +0400

    Update allowedOrigins to use 127.0.0.1

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 5000;
 
 const CLIENT_URL = process.env.CLIENT_URL || "http://localhost:3000" ;
 // const allowedOrigins = [process.env.CLIENT_URL || "http://localhost:3000" ];
- const allowedOrigins = ["http://localhost:5500" || "http://localhost:3000" ];
+ const allowedOrigins = ["http://127.0.0.1:5500" || "http://localhost:3000" ];
 // app.use(cors({
 //   origin: CLIENT_URL, // Yalnız bu URL-dən gələn sorğulara icazə ver
 //   credentials: true, // Sorğularla birlikdə çerezlərin (cookies) göndərilməsinə icazə ver
