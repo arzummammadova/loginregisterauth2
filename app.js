7227821e4dba7d92ca02da59178508086da8ec7c
Arzu Mammadova <arzuam-af106@code.edu.az>
2026-01-15 16:13:27 +0400

    Modify allowedOrigins for CORS configuration
    Updated allowedOrigins to include localhost:5500.

--- app.js
+++ app.js
@@ -14,9 +14,9 @@ import applicationRouter from './src/routers/applicationRouter.js';
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const CLIENT_URL = process.env.CLIENT_URL || "http://localhost:3000";
-const allowedOrigins = [process.env.CLIENT_URL || "http://localhost:3000"];
-
+const CLIENT_URL = process.env.CLIENT_URL || "http://localhost:3000" ;
+// const allowedOrigins = [process.env.CLIENT_URL || "http://localhost:3000" ];
+ const allowedOrigins = ["http://localhost:5500" || "http://localhost:3000" ];
 // app.use(cors({
 //   origin: CLIENT_URL, // Yalnız bu URL-dən gələn sorğulara icazə ver
 //   credentials: true, // Sorğularla birlikdə çerezlərin (cookies) göndərilməsinə icazə ver
